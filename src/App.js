e641ebca6f72d8e22a2e8a5e503072baf6bf372a
mustafa695 <amustufa37@gmail.com>
2024-10-01 16:38:26 +0500

    remove auth api

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
-import { getMe } from "./api";
 import AuthGuard from "./helper/authGuard";
 
 const App = () => {
-  const user =
-    (window.localStorage != undefined &&
-      JSON.parse(localStorage.getItem("userData"))) ||
-    null;
-
-  //Get Logged in user
-  const fetchMe = async () => {
-    try {
-      const response = await getMe();
-      if (typeof localStorage !== "undefined") {
-        localStorage.setItem("userData", JSON.stringify(response.data?.data));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMe();
-  }, []);
-
   return (
     <Router>
       <Routes>
