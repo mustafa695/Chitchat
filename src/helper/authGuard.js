7690ff8f6d7aa6ca1cf52c13401b6034b8db8fa4
mustafa695 <amustufa37@gmail.com>
2024-10-01 16:40:36 +0500

    add event listner for localstorage

--- src/helper/authGuard.js
+++ src/helper/authGuard.js
@@ -1,12 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
-const AuthGuard = ({ children }) => {
+const AuthGuard = ({ children, restrictedPaths }) => {
   const [user, setUser] = useState(() => {
     // Initialize state from localStorage
-    return JSON.parse(localStorage.getItem("userData"));
+    return JSON.parse(localStorage.getItem("userData")) || null;
   });
 
+  useEffect(() => {
+    const handleStorageChange = (event) => {
+      // Check if the changed item is "userData"
+      if (event.key === "userData") {
+        const newUserData = JSON.parse(event.newValue);
+        setUser(newUserData);
+      } else {
+        setUser(null);
+      }
+    };
+
+    // Add event listener for storage changes
+    window.addEventListener("storage", handleStorageChange);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
 
   // If user is not logged in, redirect to login page
   if (!user) {
